01564a63cab78a3eb7b8cee4c851333459a9a330
Anderson Ore <u202011467@upc.edu.pe>
2025-06-11 16:11:14 -0500

    upgrade to java 21 , flutter 3.17>

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -18,7 +18,7 @@ class _LoginState extends State<Login> {
   bool isChecked = false;
   final storage = FlutterSecureStorage();
   late String _email;
-  late  String _password;
+  late String _password;
   late loginModel creds;
 
   @override
@@ -26,7 +26,6 @@ class _LoginState extends State<Login> {
     return Scaffold(
       backgroundColor: Colors.white,
       body: ListView(
-
         children: <Widget>[
           Column(
             mainAxisAlignment: MainAxisAlignment.center,
@@ -35,125 +34,105 @@ class _LoginState extends State<Login> {
                 image: AssetImage("lib/assets/Logo.JPG"),
                 height: 120.0,
               ),
-              Padding(
-                  padding: EdgeInsets.only(top: 20.0)
-              ),
+              Padding(padding: EdgeInsets.only(top: 20.0)),
               Text(
                 'EzGym',
-                style: TextStyle(
-                    fontSize: 35.0
-                ),
+                style: TextStyle(fontSize: 35.0),
               ),
               Form(
                   child: Container(
-                    padding: EdgeInsets.all(25),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
+                padding: EdgeInsets.all(25),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    TextFormField(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Correo electrónico',
+                        ),
+                        onChanged: (valor) {
+                          _email = valor;
+                        }),
+                    Padding(padding: EdgeInsets.only(top: 20.0)),
+                    TextFormField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Contraseña',
+                      ),
+                      onChanged: (valor) {
+                        _password = valor;
+                      },
+                    ),
+                    Padding(padding: EdgeInsets.only(top: 20.0)),
+                    Row(
+                      // mainAxisAlignment: MainAxisAlignment.center,
                       children: <Widget>[
-                        TextFormField(
-                          decoration: InputDecoration(
-                            border: OutlineInputBorder(),
-                            labelText: 'Correo electrónico',
-                          ),
-                          onChanged: (valor){
-                            _email  = valor;
-                          }
-                        ),
-                        Padding(
-                            padding: EdgeInsets.only(top: 20.0)
-                        ),
-                        TextFormField(
-                          obscureText: true,
-                          decoration: InputDecoration(
-                            border: OutlineInputBorder(),
-                            labelText: 'Contraseña',
-                          ),
-                          onChanged: (valor){
-                            _password  = valor;
+                        Checkbox(
+                          value: isChecked,
+                          activeColor: Colors.indigo.shade900,
+                          onChanged: (bool? value) {
+                            setState(() {
+                              isChecked = value!;
+                            });
                           },
                         ),
-                        Padding(
-                            padding: EdgeInsets.only(top: 20.0)
+                        Text(
+                          'Recordarme',
+                          style: TextStyle(fontFamily: 'FredokaOne'),
                         ),
-                        Row(
-                          // mainAxisAlignment: MainAxisAlignment.center,
-                          children: <Widget>[
-                            Checkbox(
-                              value: isChecked,
-                              activeColor: Colors.indigo.shade900,
-                              onChanged: (bool? value) {
-                                setState(() {
-                                  isChecked = value!;
-                                });
-                              },
-                            ),
-                            Text(
-                              'Recordarme',
-                              style: TextStyle(
-                                  fontFamily: 'FredokaOne'
-                              ),
-                            ),
-                          ],
-                        ),
-                        Padding(
-                            padding: EdgeInsets.only(top: 20.0)
-                        ),
+                      ],
+                    ),
+                    Padding(padding: EdgeInsets.only(top: 20.0)),
+                    MaterialButton(
+                      height: 45.0,
+                      minWidth: 160.0,
+                      color: Colors.indigo.shade900,
+                      textColor: Colors.white,
+                      child: Text("Iniciar sesión"),
+                      onPressed: () {
+                        loggear();
+                      },
+                      splashColor: Colors.white,
+                    ),
+                    Padding(padding: EdgeInsets.only(top: 20.0)),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
                         MaterialButton(
-                          height: 45.0,
-                          minWidth: 160.0,
-                          color: Colors.indigo.shade900,
-                          textColor: Colors.white,
-                          child: Text("Iniciar sesión"),
-                          onPressed: () {
-                            loggear();
-                          },
-                          splashColor: Colors.white,
-                        ),
-                        Padding(
-                            padding: EdgeInsets.only(top: 20.0)
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: <Widget>[
-                            MaterialButton(
-                              onPressed: (){},
-                              child: Text(
-                                '¿Olvidaste tu contraseña?',
-                                style: TextStyle(
-                                    fontFamily: 'FredokaOne'
-                                ),
-                              ),
-                            )
-                          ],
-                        ),
-                        Padding(
-                            padding: EdgeInsets.only(top: 80.0)
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: <Widget>[
-                            Text(
-                              '¿Aún no tienes una cuenta?',
-                              style: TextStyle(
-                                  fontFamily: 'FredokaOne'
-                              ),
-                            ),
-                            MaterialButton(
-                              onPressed: (){
-                                Navigator.push(context, MaterialPageRoute(builder: (context)=> Create()));
-                              },
-                              child: Text(
-                                'Registrate',
-                                style: TextStyle(
-                                    fontFamily: 'FredokaOne'
-                                ),
-                              ),
-                            )
-                          ],
+                          onPressed: () {},
+                          child: Text(
+                            '¿Olvidaste tu contraseña?',
+                            style: TextStyle(fontFamily: 'FredokaOne'),
+                          ),
                         )
                       ],
                     ),
-                  ))
+                    Padding(padding: EdgeInsets.only(top: 80.0)),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text(
+                          '¿Aún no tienes una cuenta?',
+                          style: TextStyle(fontFamily: 'FredokaOne'),
+                        ),
+                        MaterialButton(
+                          onPressed: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => Create()));
+                          },
+                          child: Text(
+                            'Registrate',
+                            style: TextStyle(fontFamily: 'FredokaOne'),
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ))
             ],
           )
         ],
@@ -161,11 +140,11 @@ class _LoginState extends State<Login> {
     );
   }
 
-  void navigate(){
-    Navigator.push(context, MaterialPageRoute(builder: (context)=> Nav()));
+  void navigate() {
+    Navigator.push(context, MaterialPageRoute(builder: (context) => Nav()));
   }
 
-  void showMessage(){
+  void showMessage() {
     showDialog<String>(
       context: context,
       builder: (BuildContext context) => AlertDialog(
@@ -182,26 +161,20 @@ class _LoginState extends State<Login> {
       ),
     );
   }
-  Future<void> loggear() async{
 
+  Future<void> loggear() async {
     creds = loginModel(email: _email, password: _password);
     var json = creds.toJson();
 
     var response = await authApi.login(json);
     var decoded = jsonDecode(response.body);
 
-    if(response.statusCode == 200){
+    if (response.statusCode == 200) {
       print(decoded['id']);
       await storage.write(key: 'id', value: decoded['id']);
       navigate();
-    }
-    else{
+    } else {
       showMessage();
     }
-
   }
-
-
 }
-
-
