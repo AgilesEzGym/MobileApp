21aa0188fd27cfa1946e1807dc9249112f663df4
Anderson Ore <u202011467@upc.edu.pe>
2025-06-15 15:13:49 -0500

    update: api cloud host URL

--- lib/services/authApi.dart
+++ lib/services/authApi.dart
@@ -1,11 +1,11 @@
 import 'dart:convert';
+import 'package:ezgym/services/api_config.dart';
 import 'package:http/http.dart' as http;
 
 class authApi {
   static Future<dynamic> login(dynamic creds) async {
     final headers = {"Content-type": "application/json"};
-    const url = 'http://10.0.2.2:8080/api/v1/users/login';
-    final uri = Uri.parse(url);
+    final uri = Uri.parse('${ApiConfig.baseUrl}/api/v1/users/login');
     final json = jsonEncode(creds);
     final response = await http.post(uri, headers: headers, body: json);
     return response;
@@ -13,8 +13,7 @@ class authApi {
 
   static Future<dynamic> register(dynamic creds) async {
     final headers = {"Content-type": "application/json"};
-    const url = 'http://10.0.2.2:8080/api/v1/users/register';
-    final uri = Uri.parse(url);
+    final uri = Uri.parse('${ApiConfig.baseUrl}/api/v1/users/register');
     final json = jsonEncode(creds);
     //print(json);
     final response = await http.post(uri, headers: headers, body: json);
@@ -23,8 +22,7 @@ class authApi {
 
   static Future<dynamic> createSub(dynamic sub) async {
     final headers = {"Content-type": "application/json"};
-    const url = 'http://10.0.2.2:8080/api/v1/subscriptions';
-    final uri = Uri.parse(url);
+    final uri = Uri.parse('${ApiConfig.baseUrl}/api/v1/subscriptions');
     final json = jsonEncode(sub);
     //print(json);
     final response = await http.post(uri, headers: headers, body: json);
